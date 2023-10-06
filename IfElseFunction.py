2c0df87f1ef6a8acff7c0e4ef3ab6f1ecaeec05b
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-10-06 09:58:20 +0100

    Added Loop exercises

--- IfElseFunction.py
+++ IfElseFunction.py
@@ -6,6 +6,8 @@ def find_middle_char (my_string):
     else: return my_string[length //2]
         
 
-print (find_middle_char('abde'))
+print (find_middle_char('abcde'))
 
-
+# this calculation finds the middle character of a string by working out if the string has:
+#   an even number of characters (None will be returned) as can be divided by 2 with no remainder
+#   an odd number of characters (the middle character will be returned) has a remainder of 1 when divided by 2
---
+++ LoopRunningTotal.py
@@ -0,0 +1,9 @@
+number_list = [5, 15, 30, 50]
+result = 0
+
+for number in number_list:
+    result += number # the same as: result = result + number
+
+print(result)
+
+# this loops through the number list, adding to the running total
